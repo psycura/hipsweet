34d58d482b605e0764d370491f301f49c3a5f529
psycura <psycura@gmail.com>
2016-12-10 23:20:52 +0200

    cleaned

--- js/main.js
+++ js/main.js
@@ -1,18 +1,17 @@
 $(function () {
+    /* ---------- jquery mask for CF ----------*/
     $('#phone').mask('8 (000) 000-00-00');
+    
+    /* ---------- faq accordeon ----------*/
     $('.faq__link').on('click', function (e) {
         e.preventDefault();
         let $this = $(this),
             item = $this.closest('.faq__item');
-        
         if (!item.hasClass('active')) {
-            
             item.addClass('active')
                 .siblings()
                 .removeClass('active');
-            
         } else
             item.removeClass('active');
     })
-})
-;
+});
